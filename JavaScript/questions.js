7fcc1542c58de7c3d4b45d19ca3f10dfc1508fbb
HaripriyaBollam <haripriyabollam20@gmail.com>
2025-05-01 23:30:13 +0530

    first commit

---
+++ JavaScript/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb:1,
+        question: "What does HTML stand for?",
+        answer:"Hyper Text Markup Language",
+        options:[
+            "Hyper Text preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Text Multi Language"
+        ]
+    },
+    {
+        numb:2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options:[
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet",
+            "Common Style Sheet"
+        ]
+    },
+    {
+        numb:3,
+        question: "What does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        options:[
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Hypertext Preprocessor",
+            "Hometext Preprocessor"
+        ]
+    },
+    {
+        numb:4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options:[
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Query Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb:5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options:[
+            "eXtensible Markup Language",
+            "eXcutable Markup Language",
+            "eXTra Multiporgram Language",
+            "eXamine Multiple Language"
+        ]
+    }
+]
